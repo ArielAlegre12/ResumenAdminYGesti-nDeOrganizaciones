a2442dd84472b4d0e15a6c88e39422a5afd34599
ArielAlegre12 <114325019+ArielAlegre12@users.noreply.github.com>
2026-03-09 18:41:16 -0300

    s

--- js/navComponent.js
+++ js/navComponent.js
@@ -1,9 +1,34 @@
-class MiNav extends HTMLElement{
-    connectedCallBack(){
+//creo mi pequeño nav
+class MiNavBar extends HTMLElement {
+    connectedCallback(){
         this.innerHTML = `
-        <nav>
-        <a href="#">Inicio</a>
-        <a href="primeraParte.html">1ra Parte</a>`;
+        <nav class="nav-principal">
+            <ul class="nav-links">
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="primeraParte.html">1°Parte</a></li>
+                <li><a href="primeraParte.html">2°Parte</a></li>
+                <li><a href="primeraParte.html">3°Parte</a></li>
+                <li><a href="primeraParte.html">4°Parte</a></li>
+            </ul>
+        </nav>
+        `;
     }
 }
-customElements.define('mi-nav', MiNav);
+//creo mi footer 
+class miFooterBar extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML=`
+        <footer>
+        <p>&copy; 2026 Resumen de Administración y Gestión de Organizaciones</p>
+        <p>Desarrollado por Ariel </p>
+        <a href="https://instagram.com/alegreariel666" target="_blank">
+            <img src="img/instagram.ico" alt="Instagram">
+        </a>
+
+    </footer>
+        `;
+    }
+}
+
+customElements.define('mi-nav', MiNavBar);
+customElements.define('mi-footer', miFooterBar);
